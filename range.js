57fe753c9a8b0e4e7760fdc11a7cc1b2fc99fbca
Aymendz46 <stormaymen@gmail.com>
2022-07-30 21:44:02 +0100

    Added the sidebar, 2 range sliders, one from scratch, the other from Jquery

--- range.js
+++ range.js
@@ -1,13 +1,17 @@
 $(function () {
   $("#slider-range").slider({
     range: true,
-    min: 0,
-    max: 500,
+    min: 0.25,
+    max: 40,
     //values: [ 75, 300 ],
+    values:[0.25, 40],
+
     slide: function (event, ui) {
       $("#amount").val("$" + ui.values[0]); //+ " - $" + ui.values[ 1 ] );
     },
   });
   $("#amount").val("$" + $("#slider-range").slider("values", 0)); // +
   // " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  // $("#amount").val("$" + $("#slider-range").slider("values", 5)); // +
+
 });
