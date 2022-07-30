63c127ab048be765f8ba4df223e2bfe71af37262
Aymendz46 <stormaymen@gmail.com>
2022-07-30 23:56:58 +0100

    User profile dropdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let hide = document.querySelector("a.hide");
 let filter = document.querySelector("a.filter");
 let sidebar = document.querySelector(".side-menu");
+let profile = document.querySelector(".dropdown")
+let arrow = document.querySelector(".arrow-down")
 
 hide.onclick = () => 
 {
@@ -10,4 +12,10 @@ hide.onclick = () =>
 filter.onclick = () =>
 {
     sidebar.style.transform = "translateX(0px)";
+}
+
+arrow.onclick = () =>
+{
+    profile.style.height = "220px";
+    profile.style.display = "flex";
 }
