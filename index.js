0ef4c50c7796828a560697fef0d4bd060dc807f6
Aymendz46 <stormaymen@gmail.com>
2022-08-06 20:28:57 +0100

    made the main menu responsive

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
+const classes = [
+    '.hamburger',
+    '.close',
+    '.links',
+    '.login',
+    'header',
+];
+
+let d = document.querySelectorAll(classes);
+let logo = document.querySelector("header svg path");
+
+/* sidebar vars */
 let hide = document.querySelector("a.hide");
 let filter = document.querySelector("a.filter");
 let sidebar = document.querySelector(".side-menu");
 let profile = document.querySelector(".dropdown")
 let arrow = document.querySelector(".arrow-down")
+
+
+/* hamburger menu */
+d[2].onclick = () =>
+{
+    if (d[3].classList[1] == 'active')
+    {
+        d.forEach(el => el.classList.toggle('active'));
+        //header.style.background = "#203354";
+        //logo.style.fill = "#fff";
+    }
+    else
+    {
+        d.forEach(el => el.classList.toggle('active'));
+        //header.style.background = "#fff";
+        //logo.style.fill = "#203354";
+    }
+} 
+
+d[1].onclick = () =>
+{
+    if (d[3].classList[1] == 'active')
+    {
+        d.forEach(el => el.classList.toggle('active'));
+    }
+    else
+    {
+        d.forEach(el => el.classList.toggle('active'));
+    }
+} 
+
+/* sidebar */
 
 hide.onclick = () => 
 {
